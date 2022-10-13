181a2061cae7de44c3048b2bc16c9bc6a0b2e98c
Mariia <m.goncharenkoo@icloud.com>
2022-10-13 19:12:00 -0400

    tests passed

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -5,7 +5,12 @@ import React from "react";
 import Article from "../components/Article";
 
 test("displays the text 'please pass this test'", () => {
+  
   render(<Article />);
 
-  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+  const element = screen.queryByText("please pass this test");
+
+  screen.debug(element);
+
+  expect(element).toBeInTheDocument();
 });
